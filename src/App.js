d65bd17dbcd37d3e57451512523db4f451f2a659
Joyce Liu <joyce@Joyces-MBP.fios-router.home>
2021-08-30 17:16:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const[mediaLists, setMediaLists] = useState([]);
+
+  const query = `
+  query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+  Page (page: $page, perPage: $perPage) {
+    pageInfo {
+      total
+      currentPage
+      lastPage
+      hasNextPage
+      perPage
+    }
+    media (id: $id, search: $search) {
+      id
+      bannerImage
+      description
+      title {
+        romaji
+      }
+    }
+  }
+}`;
+const variables = {
+  search: "Naruto",
+  page: 1,
+  perPage: 25
+};
+const url = 'https://graphql.anilist.co';
+const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    };
+ useEffect(()=>{
+  fetch(url, options).then((response)=>response.json())
+                   .then((data)=>{
+                    setMediaLists([...data.data.Page.media]);
+                   })
+                   .catch((e)=>{
+                     console.log(e)
+                   });
+},[])
+
+console.log(mediaLists);
+
+  return(
+    <div className = "cards-container">
+      {
+        mediaLists.map((item, index)=>(
+          <div className = "card-container">
+              <img src = {item.bannerImage}/>
+              <p>{item.description}</p>
+          </div>
+        ))
+      }
     </div>
-  );
+  )
 }
 
 export default App;
